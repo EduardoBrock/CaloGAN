fc2e20c1e5308c245807e57ccdc6a4eae28f4300
Eduardo Brock <eduardo.gbrock@gmail.com>
2021-06-09 05:47:21 -0300

    sqfull20

--- generation/src/RunAction/SQFULL20_RunAction.cc
+++ generation/src/RunAction/SQFULL20_RunAction.cc
@@ -76,7 +76,7 @@ RunAction::RunAction()
 
   analysisManager->CreateNtuple(fname.c_str(), "Edep and TrackL");
 
-  int total_bins = 520 + 3;  // 3 overflow bins for the three calo layers
+  int total_bins = 1200 + 3;  // 3 overflow bins for the three calo layers
 
   for (int i = 0; i < total_bins; ++i) {
 
