13ef74f87dc732afa326040653b105f70bc5ece3
Eduardo Brock <eduardo.gbrock@gmail.com>
2021-06-09 04:58:40 -0300

    Update lxplus-env.sh

--- generation/cfg/lxplus-env.sh
+++ generation/cfg/lxplus-env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 source /cvmfs/sft.cern.ch/lcg/contrib/gcc/6.3.0/x86_64-slc6/setup.sh
-source /cvmfs/geant4.cern.ch/geant4/10.6/x86_64-slc6-gcc63-optdeb-MT/CMake-setup.sh
-source /cvmfs/geant4.cern.ch/geant4/10.6/x86_64-slc6-gcc63-optdeb-MT/share/Geant4-10.6.0/geant4make/geant4make.sh
-source /cvmfs/geant4.cern.ch/geant4/10.6/x86_64-slc6-gcc63-optdeb-MT/bin/geant4.sh 
+source /cvmfs/geant4.cern.ch/geant4/10.6/x86_64-slc6-gcc63-optdeb/CMake-setup.sh
+source /cvmfs/geant4.cern.ch/geant4/10.6/x86_64-slc6-gcc63-optdeb/share/Geant4-10.6.0/geant4make/geant4make.sh
+source /cvmfs/geant4.cern.ch/geant4/10.6/x86_64-slc6-gcc63-optdeb/bin/geant4.sh
